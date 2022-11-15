7e9154124e7bef6fe9d8cc953b919027eb2d0cf5
Mateusz Martin <mateusz.martinn@gmail.com>
2022-11-15 15:00:54 +0100

    initial Build

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.js";
+
+// Change code below this line
+
+// Defining DOM elements
+
+const gallery = document.querySelector(".gallery");
+
+// creating gallery items and images
+function createGallery() {
+  for (const image in galleryItems) {
+    // creating gallery elements
+    const photo = document.createElement("img");
+    const galleryItem = document.createElement("a");
+
+    galleryItem.classList.add("gallery__item");
+
+    // appending galleryItem to gallery and galleryImage to galleryItem
+    gallery.appendChild(galleryItem);
+    galleryItem.appendChild(photo);
+
+    // setting classes and attributes to each photo
+    photo.classList.add("gallery__image");
+    photo.setAttribute(`src`, `${galleryItems[image].preview}`);
+    photo.setAttribute("alt", `${galleryItems[image].description}`);
+    galleryItem.setAttribute(`href`, `${galleryItems[image].original}`);
+  }
+}
+
+// calling functions
+createGallery();
+
+// creating slideshow form gallery's elements
+var lightbox = new SimpleLightbox(".gallery a", {});
+
+// calling modal with original image's size
+gallery.addEventListener(`click`, (event) => {
+  if (event.target.tagName.toLowerCase() !== "a") {
+    return;
+  }
+  // find object in 'galleryItems' array by preview's src, then get that object's 'original' value
+  const originalImage = event.target.getAttribute("href");
+
+  // show modal
+  lightbox.open();
+});
---
+++ js/02-video.js
@@ -0,0 +1,12 @@
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector("#vimeo-player");
+const player = new Vimeo.Player(iframe);
+
+player.on("play", () => {
+  console.log("played the video!");
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log("title:", title);
+});
